3c9b60afa6368d9db6cf1bef302f880de7b9fc83
Anagha Joshi <AnaghaJ2@citiustech.com>
2021-07-12 22:39:41 +0530

    Remove send note component

--- impact-frontend/src/app/modules/notes/note-routing.module.ts
+++ impact-frontend/src/app/modules/notes/note-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { NotesComponent } from './notes.component';
 import { ReceiveNoteComponent } from './receive-note/receive-note.component';
-import { SendNoteComponent } from './send-note/send-note.component';
 import { SentNoteComponent } from './sent-note/sent-note.component';
 
 const routes: Routes = [
@@ -10,7 +9,7 @@ const routes: Routes = [
         path: '',
         component: NotesComponent,
         children: [
-            { path: '', component: SendNoteComponent },
+            { path: '', component: NotesComponent },
             { path: 'received-notes', component: ReceiveNoteComponent },
             { path: 'sent-notes', component: SentNoteComponent }
         ]
--- impact-frontend/src/app/modules/notes/notes.component.ts
+++ impact-frontend/src/app/modules/notes/notes.component.ts
@@ -34,7 +34,7 @@ export class NotesComponent implements OnInit {
     this.noteService.getUsersByRole().subscribe(val => {
       this.users = val.filter(user => {
         // TODO: sender id is hardcoded for now . would be fetched from session
-        return user.userId !== parseInt(sessionStorage.getItem("userId"));
+        return user.userId !== parseInt(localStorage.getItem("userId"));
       });
       console.log(val);
       
@@ -49,7 +49,7 @@ export class NotesComponent implements OnInit {
     
     let newNotes: Notes = this.form.value;
     // TODO: sender id is hardcoded for now . would be fetched from session
-    newNotes.senderId = parseInt(sessionStorage.getItem("userId"));
+    newNotes.senderId = parseInt(localStorage.getItem("userId"));
     this.noteService.sendNotes(newNotes).subscribe(
       data => {
         this.toastService.show(data.message, { classname: 'bg-success text-light', delay: 5000 })
